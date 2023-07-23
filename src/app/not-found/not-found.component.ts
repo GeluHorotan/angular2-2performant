879a5bcb28bddc7cd67f882908ca83408e718772
saboumariusbogdan <100289634+GeluHorotan@users.noreply.github.com>
2023-07-23 21:24:16 +0300

    feat: add 404 page not found logic

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit, OnDestroy {
+  remainingSeconds: number = 10;
+  private timer: any;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.countdown();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  countdown(): void {
+    if (this.remainingSeconds > 0) {
+      this.remainingSeconds--;
+    } else {
+      this.goHome();
+    }
+  }
+
+  goHome(): void {
+    this.router.navigate(['/']);
+  }
 }
