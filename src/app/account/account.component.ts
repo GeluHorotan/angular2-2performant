6b0a61e78eee77988fdf0874180c2dd317be7f59
saboumariusbogdan <100289634+GeluHorotan@users.noreply.github.com>
2023-07-23 19:50:53 +0300

    feat: create account page and implement the functionality

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  user: any;
+  currentTime!: { date: Date; time: string };
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.getUser();
+    this.updateDateTime();
+    setInterval(() => this.updateDateTime(), 1000);
+  }
+
+  private updateDateTime(): void {
+    this.currentTime = {
+      date: new Date(),
+      time: new Date().toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      }),
+    };
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
